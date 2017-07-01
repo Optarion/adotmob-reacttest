58f0ba75793d729524031315fd0e0f5c9d7d46f3
Adrien Rousselet <adrien@cogitstudio.com>
2017-07-01 16:32:17 +0200

    @0.1h : Fix warning by replacing img's title with alt

--- react-basic/src/components/Item.js
+++ react-basic/src/components/Item.js
@@ -6,7 +6,7 @@ export default class Item extends Component {
 		return(
 			<div className="column is-one-third Item">
 				<a href={this.props.content.url}>{this.props.content.url}</a>
-				<img src={this.props.content.thumbnailUrl} title={this.props.content.title}/>
+				<img src={this.props.content.thumbnailUrl} alt={this.props.content.title} />
 			</div>
 		);
 	}

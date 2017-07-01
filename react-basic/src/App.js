7fefb6ae4d6b585a75d718087a9a18f26bb18c5b
Adrien Rousselet <adrien@cogitstudio.com>
2017-07-01 15:59:40 +0200

    @0.3h : Update App with server request

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -2,6 +2,40 @@ import React, { Component } from 'react'
 import ItemsList from './components/ItemsList'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items : []
+    }
+  }
+
+  componentDidMount(){
+    this.getItems();
+  }
+
+  // getItems makes the request to get the datas from the servers and update the state with the response
+  getItems() {
+    var url = "https://jsonplaceholder.typicode.com/photos";
+    var xmlhttp = new XMLHttpRequest();
+
+    var itemsRequest =  new Promise(function(resolve, reject){
+      xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+          if (xmlhttp.status == 200) {
+            resolve(JSON.parse(xmlhttp.response));
+          }
+        }
+      }
+
+      xmlhttp.open("GET", url, true);
+      xmlhttp.send(); 
+    }).then((datas) => {
+        this.setState({
+          items : datas
+        })
+      })
+  }
+
   render() {
     //We take only the 100 first items
     var items = this.state.items.slice(0,100);

5dc969d678484840c4ded17094d5781eb65c89a6
Adrien Rousselet <adrien@cogitstudio.com>
2017-07-01 16:31:08 +0200

    @0.2h : Add bulma's grid classes to Item & ItemsList + Create Item/ItemsList own stylesheets

--- react-basic/src/components/Item.js
+++ react-basic/src/components/Item.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import '../css/Item.css'
 
 export default class Item extends Component {
 	render() {
 		return(
-			<div className="Item">
+			<div className="column is-one-third Item">
 				<a href={this.props.content.url}>{this.props.content.url}</a>
 				<img src={this.props.content.thumbnailUrl} title={this.props.content.title}/>
 			</div>
--- react-basic/src/components/ItemsList.js
+++ react-basic/src/components/ItemsList.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import Item from './Item'
+import '../css/ItemsList.css'
 
 export default class ItemsList extends Component {
 	render() {
 		var items = this.props.items;
 		return(
-			<div className="ItemsList">
+			<div className="columns ItemsList">
 				{items.map(function(data){
 					return <Item key={data.id} content={data}/>
 				})}
